4bda9b711f1bf2ced2112d9f64cc3eb600edd302
codingMaz <m.balandari.toroghi@gmail.com>
2023-05-24 15:05:31 +0330

    first

---
+++ main.py
@@ -0,0 +1,14 @@
+import urllib.request
+import urllib.parse
+import urllib.error
+import zipfile
+import io
+import struct
+
+url = "https://github.com/GeospatialPython/Learn/raw/master/hancock.zip"
+cloudshape = urllib.request.urlopen(url)
+memoryshape = io.BytesIO(cloudshape.read())
+zipshape = zipfile.ZipFile(memoryshape)
+cloudshp = zipshape.read("hancock.shp")
+# Access Python string as an array
+print(struct.unpack("<dddd", cloudshp[36:68]))
