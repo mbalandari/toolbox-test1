d58498fb53f17321b74ac27289a8273aac6f1f42
codingMaz <m.balandari.toroghi@gmail.com>
2023-05-25 10:34:00 +0330

    get the first coordinates

--- parsers.py
+++ parsers.py
@@ -2,4 +2,6 @@ from xml.dom import minidom
 
 kml = minidom.parse("time-stamp-point.kml")
 PlaceMarks = kml.getElementsByTagName("Placemark")
-print(len(PlaceMarks))
+coordinates = PlaceMarks[0].getElementsByTagName("coordinates")
+point = coordinates[0].firstChild.data
+print(point)
