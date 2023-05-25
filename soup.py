185dcee38f0a12aae428654c991e38129282eb3b
codingMaz <m.balandari.toroghi@gmail.com>
2023-05-25 11:41:45 +0330

    fix broken data

--- soup.py
+++ soup.py
@@ -2,3 +2,6 @@ from bs4 import BeautifulSoup
 
 gpx = open("broken_data.gpx")
 soup = BeautifulSoup(gpx.read(), features="xml")
+fixed = open("fixed_data.gpx", "w")
+fixed.write(soup.prettify())
+fixed.close()
