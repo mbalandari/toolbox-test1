3b8429717a02f6a94bfc2216ddb5789571628704
codingMaz <m.balandari.toroghi@gmail.com>
2023-05-25 10:39:20 +0330

    use python list comprehension

--- parsers.py
+++ parsers.py
@@ -4,8 +4,6 @@ kml = minidom.parse("time-stamp-point.kml")
 PlaceMarks = kml.getElementsByTagName("Placemark")
 coordinates = PlaceMarks[0].getElementsByTagName("coordinates")
 point = coordinates[0].firstChild.data
-x, y, z = point.split(",")
-x = float(x)
-y = float(y)
-z = float(z)
+# python list comprehension
+x, y, z = [float(c) for c in point.split(",")]
 print(x, y, z)
