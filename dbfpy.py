2f77e09c5d21a30e436d2e61aee4dbc8c4a4e001
codingMaz <m.balandari.toroghi@gmail.com>
2023-05-25 14:00:03 +0330

    edit with dbfpy3

--- dbfpy.py
+++ dbfpy.py
@@ -1,4 +1,9 @@
 from dbfpy3 import dbf
 
 db = dbf.Dbf("GIS_CensusTract_poly.dbf")
-print(db[0])
+rec = db[0]
+field = rec["POPULAT10"]
+rec["POPULAT10"] = field + 1
+rec.store()
+del rec
+print(db[0]["POPULAT10"])
